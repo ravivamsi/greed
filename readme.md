c5cf1fc46231fa9aa3608026857409cb410bb7e3
Vamsi Ravi <ravivamsi@yahoo.com>
2020-06-26 10:38:57 -0500

    Springboot Initialized

---
+++ readme.md
@@ -0,0 +1,15 @@
+# greed 
+
+Greedy Algorithm powered by Alpaca API 
+
+## Technical Specifications
+
+- Maven 
+- JDK 1.8 
+
+## API
+
+Refer to the Postman Collection
+
+
+ 
---
+++ src/test/java/com/greedpro/TraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greedpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
